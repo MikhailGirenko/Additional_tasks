23ca5d29e9e2015794fc5b0f2eb8afacfb65b333
Mikhail Girenko <misha15girenko@gmail.com>
2022-12-11 16:58:18 +0300

    2 commit

--- ru.girenko.springcourse/src/main/java/ru/girenko/springcourse/TestSpring.java
+++ ru.girenko.springcourse/src/main/java/ru/girenko/springcourse/TestSpring.java
@@ -8,8 +8,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic2.getSong());
+
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean comparison = musicPlayer1 == musicPlayer2;
+//        System.out.println(comparison);
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//        musicPlayer1.setVolume(10);
+//        //System.out.println(musicPlayer1.getName());
+//        System.out.println(musicPlayer1.getVolume());
+//        //System.out.println(musicPlayer2.getName());
+//        System.out.println(musicPlayer2.getVolume());
+//       // musicPlayer.playMusicList();
         context.close();
     }
 }
